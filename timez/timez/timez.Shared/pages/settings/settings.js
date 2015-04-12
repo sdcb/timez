6278d59b3d2c7d9c09e40701decc13ed32e7e083
Flysha <sdflysha@qq.com>
2015-04-12 22:50:44 +0800

    no bug

--- timez/timez/timez.Shared/pages/settings/settings.js
+++ timez/timez/timez.Shared/pages/settings/settings.js
@@ -25,18 +25,38 @@
         });
     }
 
-    function ViewModel(tz) {
+    var mode = {
+        edit: 1,
+        create: 2
+    };
+
+    function ViewModel(tz, data) {
         var me = this;
         this.tz = tz;
-        this.item = new TimezApp.TimeItem('新的时间', moment.duration(0), this.tz.time);
+        this.mode = data ? mode.edit : mode.create;
+        this.item = data ? data : new TimezApp.TimeItem('新的时间', moment.duration(0), this.tz.time);
 
         this.time = new Hms(this.item.offset());
         this.time.duration.subscribe(function(v) {
             me.item.offset(v);
         });
-        this.save = function() {
-            me.tz.times.push(me.item);
+        this.save = function () {
+            if (this.mode === mode.edit) {
+            } else if (this.mode === mode.create) {
+                me.tz.times.push(me.item);
+            }
+            me.tz.save();
             WinJS.Navigation.back();
+        }
+        this.drop = function() {
+            var msg = new Windows.UI.Popups.MessageDialog('确定要删除此项吗？');
+            msg.commands.push(new Windows.UI.Popups.UICommand('确定', function() {
+                me.tz.drop(me.item);
+            }));
+            msg.commands.push(new Windows.UI.Popups.UICommand('取消'));
+            msg.showAsync().then(function() {
+                WinJS.Navigation.back();
+            });
         }
 
         this.p12 = [];
@@ -50,7 +70,7 @@
 
     WinJS.UI.Pages.define("/pages/settings/settings.html", {
         ready: function (element, options) {
-            window.vm = new ViewModel(options.tz);
+            window.vm = new ViewModel(options.tz, options.data);
             ko.applyBindings(vm, element);
             document.getElementById("appbar").winControl.disabled = false;
         },
